d5d6bfd91e43bcff160d0157918e0e7d45cc0bf4
bekbolat <bekbolatjabbarbergenov@gmail.com>
2025-01-07 16:56:55 +0500

    just commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from "react";
 
 function App() {
-    const [userId, setUserId] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [name, setName] = useState(null);
 
-    useEffect(() => {
-        const tg = window.Telegram.WebApp;
-        tg.expand();
+  useEffect(() => {
+    const tg = window.Telegram.WebApp;
+    tg.expand();
 
+    const user_id = tg.initDataUnsafe.user?.id;
+    const name = tg.initDataUnsafe.user.username;
+    setName(name);
+    setUserId(user_id);
 
-        // Получение user_id
-        const user_id = tg.initDataUnsafe.user?.id;
-        setUserId(user_id); // Устанавливаем user_id в состояние
+    console.log("User ID:", user_id);
+    console.log(tg.initDataUnsafe);
+  }, []);
 
-        console.log("User ID:", user_id);
-        console.log(tg.initDataUnsafe);
-        
-    }, []);
-
-    return (
-        <div style={{ textAlign: "center", padding: "20px" }}>
-            <h1>{userId ? `Ваш ID: ${userId}` : "Загрузка..."}</h1>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>{userId ? `Ваш ID: ${userId}` : "Загрузка..."}</h1>
+      <h1>{name}</h1>
+    </div>
+  );
 }
 
 export default App;
